e25ae4169c08d1e0e6188b76dfe65e356694b04c
oNinjaZ <marcbelzamour@yahoo.com>
2022-05-30 22:01:39 -0400

    Add Reviewer model

---
+++ BookReviewer.Api/Models/Reviewer.cs
@@ -0,0 +1,9 @@
+namespace BookReviewer.Api.Models;
+
+public class Reviewer 
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    
+}
