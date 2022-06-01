0016548afd3f2f46bb5dd14d79cb17eccae8f470
Marc B <ninjaz.gg@outlook.com>
2022-06-01 16:01:36 -0400

    make publishDate nullable

--- BookReviewer.Api/Models/Book.cs
+++ BookReviewer.Api/Models/Book.cs
@@ -4,8 +4,8 @@ public class Book
 {
     public int Id { get; set; }
     public string? Title { get; set; }
-    public DateTime PublishDate { get; set; }
-    public int PageCount { get; set; }
+    public DateTime? PublishDate { get; set; }
+    public int? PageCount { get; set; }
     public Author? Author { get; set; }
     public ICollection<Review>? Reviews { get; set; }
     public ICollection<BookGenre>? BookGenres { get; set; }
