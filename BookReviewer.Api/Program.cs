458ca7afa9cdbfb84bbb3a19cf8a9d4b10ff832b
Marc B <ninjaz.gg@outlook.com>
2022-06-01 16:51:39 -0400

    fix format

--- BookReviewer.Api/Program.cs
+++ BookReviewer.Api/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<DataContext>(opts =>
 {
     opts.UseSqlite(builder.Configuration.GetValue<string>("ConnectionStrings:Sqlite"));
