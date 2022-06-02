cc2cdd2bd76b9e4d5baf60c01ecafefc3813306f
oNinjaZ <marcbelzamour@yahoo.com>
2022-06-01 23:43:30 -0400

    add 2 get routes

--- BookReviewer.Api/Endpoints/BookEndpoints.cs
+++ BookReviewer.Api/Endpoints/BookEndpoints.cs
@@ -16,8 +16,23 @@ public static class BookEndpoints
         app.MapGet("/books", async (IBookRepository bookRepo) =>
         {
             var books = await bookRepo.GetBooksAsync();
-
             return Results.Ok(books);
+        });
+
+        app.MapGet("/books/{id}", async (int id, IBookRepository bookRepo) =>
+        {
+            var book = await bookRepo.GetBookAsync(id);
+            return book is not null ? Results.Ok(book) : Results.NotFound();
+        });
+
+        app.MapGet("/books/{id}/rating", async (int id, IBookRepository bookRepo) =>
+        {
+            var book = await bookRepo.GetBookAsync(id);
+            if (book is null)
+                return Results.NotFound();
+
+            var rating = await bookRepo.GetBookRatingAsync(id);
+            return Results.Ok(rating);
         });
     }
 }
