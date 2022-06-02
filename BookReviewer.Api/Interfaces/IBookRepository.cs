29ecd53650075966800e8ca42de5d0e0e7e4dc39
oNinjaZ <marcbelzamour@yahoo.com>
2022-06-01 21:18:39 -0400

    add get methods

--- BookReviewer.Api/Interfaces/IBookRepository.cs
+++ BookReviewer.Api/Interfaces/IBookRepository.cs
@@ -4,5 +4,16 @@ namespace BookReviewer.Api.Interfaces;
 
 public interface IBookRepository
 {
-    public Task<ICollection<Book>> GetBooksAsync();
+    Task<ICollection<Book>> GetBooksAsync();
+    
+    Task<Book?> GetBookAsync(int id);
+
+    Task<Book?> GetBookAsync(string title);
+
+    /// <summary>
+    /// Calculates the average review rating of a Book.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>The average rating, rounded to the nearest tenths.</returns>
+    Task<double> GetBookRatingAsync(int id);
 }
--- BookReviewer.Api/Repository/BookRepository.cs
+++ BookReviewer.Api/Repository/BookRepository.cs
@@ -12,8 +12,23 @@ public class BookRepository : IBookRepository
     {
         _context = context;
     }
-    public async Task<ICollection<Book>> GetBooksAsync()
+
+    public async Task<Book?> GetBookAsync(int id) => await _context.Book.FirstOrDefaultAsync(b => b.Id == id);
+
+    public async Task<Book?> GetBookAsync(string title)
     {
-        return await _context.Book!.OrderBy(b => b.Id).ToListAsync();
+        return await _context.Book.FirstOrDefaultAsync(b =>
+            b.Title.ToUpper() == title.ToUpper());
+    }
+
+    public async Task<ICollection<Book>> GetBooksAsync() => await _context.Book.ToListAsync();
+
+    public async Task<double> GetBookRatingAsync(int id)
+    {
+        var ratings = await _context.Review.Where(r => r.Book.Id == id).ToListAsync();
+        if (!ratings.Any())
+            return 0;
+        var ovrRating = (double)ratings.Sum(r => r.Rating) / ratings.Count();
+        return Math.Round(ovrRating, 1);
     }
 }
