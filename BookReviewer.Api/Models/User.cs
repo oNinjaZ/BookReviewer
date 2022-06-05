d4d073095250bbf19cba4c5f9763a181717936d2
oNinjaZ <marcbelzamour@yahoo.com>
2022-06-05 02:16:09 -0400

    add Email prop

--- BookReviewer.Api/Models/User.cs
+++ BookReviewer.Api/Models/User.cs
@@ -5,5 +5,6 @@ public class User
     public int Id { get; set; }
     public string Username { get; set; } = default!;
     public string Password { get; set; } = default!;
+    public string Email { get; set; } = default!;
     public ICollection<Review> Reviews { get; set; } = default!;
 }
