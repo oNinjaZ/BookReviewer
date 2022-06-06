3bc8f3397196ff2a3c4c79043208615970153190
oNinjaZ <marcbelzamour@yahoo.com>
2022-06-06 08:12:55 -0400

    update migrations

---
+++ BookReviewer.Api/Migrations/20220605064435_RenameTableToAuthorBooks.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookReviewer.Api.Migrations
+{
+    public partial class RenameTableToAuthorBooks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBooks_Author_AuthorId",
+                table: "AuthorBooks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBooks_Book_BookId",
+                table: "AuthorBooks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuthorBooks",
+                table: "AuthorBooks");
+
+            migrationBuilder.RenameTable(
+                name: "AuthorBooks",
+                newName: "AuthorBook");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuthorBooks_BookId",
+                table: "AuthorBook",
+                newName: "IX_AuthorBook_BookId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuthorBook",
+                table: "AuthorBook",
+                columns: new[] { "AuthorId", "BookId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Author_AuthorId",
+                table: "AuthorBook",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Book_BookId",
+                table: "AuthorBook",
+                column: "BookId",
+                principalTable: "Book",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Author_AuthorId",
+                table: "AuthorBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Book_BookId",
+                table: "AuthorBook");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuthorBook",
+                table: "AuthorBook");
+
+            migrationBuilder.RenameTable(
+                name: "AuthorBook",
+                newName: "AuthorBooks");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuthorBook_BookId",
+                table: "AuthorBooks",
+                newName: "IX_AuthorBooks_BookId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuthorBooks",
+                table: "AuthorBooks",
+                columns: new[] { "AuthorId", "BookId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBooks_Author_AuthorId",
+                table: "AuthorBooks",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBooks_Book_BookId",
+                table: "AuthorBooks",
+                column: "BookId",
+                principalTable: "Book",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
