0c12ee26da0c9519eb6d94f4323a9525a1f8ccf4
oNinjaZ <marcbelzamour@yahoo.com>
2022-06-10 23:31:00 -0400

    update add post/get method

--- BookReviewer.Api/Endpoints/AuthorEndpoints.cs
+++ BookReviewer.Api/Endpoints/AuthorEndpoints.cs
@@ -1,4 +1,7 @@
+using BookReviewer.Api.Dtos.Author;
+using BookReviewer.Api.Extensions;
 using BookReviewer.Api.Interfaces;
+using BookReviewer.Api.Models;
 using BookReviewer.Api.Repositories;
 
 namespace BookReviewer.Api.Endpoints;
@@ -13,22 +16,34 @@ public static class AuthorEndpoints
 
     public static void UseAuthorEndpoints(this IEndpointRouteBuilder app)
     {
+        app.MapPost("/authors", async (AuthorCreateDto dto, IAuthorRepository authorRepo) =>
+        {
+            var created = await authorRepo.CreateAuthorAsync(new Author
+            {
+                FirstName = dto.FirstName,
+                LastName = dto.LastName
+            });
+        });
+
         app.MapGet("/authors", async (IAuthorRepository authorRepo) =>
         {
             var authors = await authorRepo.GetAuthorsAsync();
             return Results.Ok(authors);
         });
 
-        app.MapPost("/authors/{authorid}/books", async (IAuthorRepository authorRepo) =>
+        app.MapGet("/authors/{id}", async (int id, IAuthorRepository authorRepo) =>
         {
-            var authors = await authorRepo.GetAuthorsAsync();
-            return Results.Ok(authors);
+            var author = await authorRepo.GetAuthorAsync(id);
+            if (author is null)
+                return Results.NotFound();
+
+            return Results.Ok(author.AsDto());
         });
 
         app.MapDelete("/authors/{id}", async (int id, IAuthorRepository authorRepo) =>
         {
-            return await authorRepo.DeleteAuthorAsync(id) 
-                ? Results.Ok() 
+            return await authorRepo.DeleteAuthorAsync(id)
+                ? Results.Ok()
                 : Results.NotFound();
         });
     }
