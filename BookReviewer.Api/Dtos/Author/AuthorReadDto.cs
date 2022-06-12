09550197a449572d40fe26e89add746871d9bbda
oNinjaZ <marcbelzamour@yahoo.com>
2022-06-12 11:19:57 -0400

    untrack .db

--- BookReviewer.Api/Dtos/Author/AuthorReadDto.cs
+++ BookReviewer.Api/Dtos/Author/AuthorReadDto.cs
@@ -1,3 +1,5 @@
+using BookReviewer.Api.Models;
+
 namespace BookReviewer.Api.Dtos.Author;
 
 public class AuthorReadDto
@@ -5,4 +7,6 @@ public class AuthorReadDto
     public int Id { get; set; }
     public string FirstName { get; set; } = default!;
     public string? LastName { get; set; }
+    public ICollection<AuthorBook>? AuthorBooks { get; set; }
+
 }
