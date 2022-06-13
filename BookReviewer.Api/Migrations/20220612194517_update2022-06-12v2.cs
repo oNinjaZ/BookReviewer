5ab414a6f318ea12f7e7210f9b6fc167da4367ef
oNinjaZ <marcbelzamour@yahoo.com>
2022-06-12 20:06:44 -0400

    add migration

---
+++ BookReviewer.Api/Migrations/20220612194517_update2022-06-12v2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookReviewer.Api.Migrations
+{
+    public partial class update20220612v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
